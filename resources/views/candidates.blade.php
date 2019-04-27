61d933c03d3389c519f112abd795c3ba7ca55555
Ogbo-Chiro <chiro.ogbo@gmail.com>
2019-04-27 17:35:21 +0200

    candidate photos

--- resources/views/candidates.blade.php
+++ resources/views/candidates.blade.php
@@ -21,9 +21,10 @@
 
 
             @foreach($runners as $run)
+              <?php $img = $run->position; ?>
 
         <div class="card col-4">
-          <img class="card-img-top" style="" src="{{ asset($run->photo) }}" alt="Card image cap">
+          <img class="card-img-top" style="" src="{{ asset('img/' . '$img' . '.png') }}" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title position-result" style="color: #8E4A49">{{ $run->position }}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{{ $run->first_name . ' ' . $run->last_name }}</h6> 
